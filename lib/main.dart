1835e3071742a56537d7a905803fa3bc88144b5c
Alexandre Tertuliano <alexandretertuliano2010@hotmail.com>
2020-12-30 20:53:38 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+
+  runApp(MyApp());
+
+ QuerySnapshot snapshot = await Firestore.instance.collection("mensagens").getDocuments();
+ snapshot.documents.forEach((d) {
+   d.reference.updateData({'lido': false});
+   });
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat Flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
